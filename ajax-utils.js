7c01522834050fbb8a9e8f0cf2aa91090f775fb5
azliep <111280184+azliep@users.noreply.github.com>
2022-11-18 12:07:33 +0800

    Add files via upload

---
+++ ajax-utils.js
@@ -0,0 +1,49 @@
+(function (global) {
+
+    //set up a message for our utility
+    var ajaxUtils ={}
+
+    //Returns an HTTP request object
+    function getRequestObject() {
+        if (window.XMLHttpRequest) {
+            return (new XMLHttpRequest());
+        }
+        else if (window.ActiveXObject) {
+            // For very old IE browsers (optional)
+            return (new ActiveXObject("Microsoft.XMLHTTP"));
+        }
+        else {
+            global.alert("Ajax is not supported!");
+            return(null);
+        }
+    }
+    
+
+    // Makes an Ajax GET request to 'requestUrl'
+    ajaxUtils.sendGetRequest = 
+        function(requestUrl, responseHandler) {
+            var request = getRequestObject();
+            request.onreadystatechange =
+                function() {
+                    handleResponse(request, responseHandler);
+                };
+            request.open("GET", requestUrl, true);
+            request.send(null); // for POST only
+        };
+
+
+    //  Only  calls user provided 'responseHandler'
+    //  function if response is ready
+    //  and not an error
+    function handleRespone(request,
+                           responseHadndler) {
+        if ((request.readyState == 4) &&
+           (request.status == 200)) {
+          responseHandler(request);
+        }
+    }
+
+    //Expose utility to the global object
+    global.$ajaxUtils = ajaxUtils;
+
+})(window);
